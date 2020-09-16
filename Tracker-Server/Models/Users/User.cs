0418f3ef70e59d8b836f5023cabf075f5ac846df
Lukas Kuppers <lkuppers11@gmail.com>
2020-09-15 18:28:12 -0700

    Add User Classes

---
+++ Tracker-Server/Models/Users/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker_Server.Models.Users
+{
+    public class User
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public UserCredentials Credentials { get; set; }
+
+        public string Username { get; set; }
+
+        public List<Guid> Projects { get; set; }
+    }
+}
---
+++ Tracker-Server/Models/Users/UserCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker_Server.Models.Users
+{
+    public class UserCredentials
+    {
+        public string Email { get; set; }
+
+        public Guid PwdSalt { get; set; }
+
+        public string PwdHash { get; set; }
+    }
+}
