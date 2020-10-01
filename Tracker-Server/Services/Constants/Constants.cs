8120830a278a0d1088a2fc3e8886cb9bb6bc7962
Lukas Kuppers <lkuppers11@gmail.com>
2020-10-01 15:05:59 -0700

    Add constants service

---
+++ Tracker-Server/Services/Constants/Constants.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Reflection;
+
+namespace Tracker_Server.Services.Constants
+{
+    // handle retrieval of constants from systemConstants.resx
+    public class Constants
+    {
+        private static readonly string RESOURCE_PATH = "Tracker-Server.Resources.SystemConstants.json";
+
+        private static JObject getJson()
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var resStream = assembly.GetManifestResourceStream(RESOURCE_PATH);
+            using (var reader = new StreamReader(resStream))
+            {
+                string jsonContent = reader.ReadToEnd();
+                JObject data = JObject.Parse(jsonContent);
+                return data;
+            }
+        }
+
+        public static string getString(string key)
+        {
+            JObject data = getJson();
+            return (string)data[key];
+        }
+    }
+}
