d763088eaeccc75e506907cf6bf057704abbf79d
Lukas Kuppers <lkuppers11@gmail.com>
2020-10-03 22:31:09 -0700

    Add ValidationFilter class

---
+++ Tracker-Server/Services/ActionFilters/ValidationFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker_Server.Services.ActionFilters
+{
+    public class ValidationFilter
+    {
+    }
+}
