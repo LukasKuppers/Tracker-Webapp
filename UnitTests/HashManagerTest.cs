9e60f2db9081e07db48c49e68d0f3fd7979f6046
Lukas Kuppers <lkuppers11@gmail.com>
2020-10-20 19:58:26 -0700

    Add Unit test package

---
+++ UnitTests/HashManagerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class HashManagerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
