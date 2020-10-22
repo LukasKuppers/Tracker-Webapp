e884c502e666ab343ede61dbc8997b40b7ba0ed8
Lukas Kuppers <lkuppers11@gmail.com>
2020-10-22 16:31:47 -0700

    Add mock resources for unit testing

---
+++ UnitTests/MockResource.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using Tracker_Server.Services.Constants;
+
+namespace UnitTests
+{
+    class MockResource : Mock<IResource>
+    {
+        public MockResource MockGetString(string key, string output)
+        {
+            Setup(x => x.GetString(
+                key
+            )).Returns(output);
+            return this;
+        }
+    }
+}
