628199af66c278ebe94281f63cbdf787cf7a9743
Lukas Kuppers <lkuppers11@gmail.com>
2020-10-21 21:21:12 -0700

    Add AuthService unit test

--- UnitTests/AuthServiceTest.cs
+++ UnitTests/AuthServiceTest.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Tracker_Server.Models.Users;
+using Tracker_Server.Services.Authorization;
+using Tracker_Server.Services.Constants;
 using Tracker_Server.Services.DataAccess;
 using Xunit;
 
@@ -12,7 +15,39 @@ namespace UnitTests
         [Fact]
         public void Test_IsValidUser_ValidUser()
         {
+            // arrange
 
+            HashManager manager = new HashManager();
+
+            string email = "Jon@bones.com";
+            string password = "ThaChamp123";
+            Guid pwdSalt = new Guid();
+            List<User> users = new List<User> 
+            { 
+                new User {  
+                    Id = new Guid(),
+                    Credentials = new UserCredentials 
+                        { 
+                            PwdHash = manager.GetHash(pwdSalt.ToString(), password, 0), 
+                            PwdSalt = pwdSalt
+                        }, 
+                    Email = email, 
+                    Username = "JonnyBonesJones",
+                    Projects = new List<Guid>()
+                } 
+            };
+
+            var mockDbClient = new MockDBClient()
+                .MockContains<User, string>(Resource.getString("db_users_path"), "Email", email, true)
+                .MockFindByField<User, string>(Resource.getString("db_users_path"), "Email", email, users);
+
+            // act
+            AuthService authService = new AuthService(mockDbClient.Object);
+            bool result = authService.IsValidUser(email, password);
+
+            // assert
+            Assert.True(result);
+            mockDbClient.VerifyAll();
         }
     }
 }
