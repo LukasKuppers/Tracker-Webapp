ef39d18f04f2cec7eefb24a117580963bafed8e3
Lukas Kuppers <lkuppers11@gmail.com>
2020-11-19 15:59:01 -0800

    Add register page UI

---
+++ Tracker-Web/Model/Register/RegisterForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tracker_Web.Model.Register
+{
+    public class RegisterForm
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(16)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
