2436da7a88520e6e09045ba59c467528bc0be4f3
Lukas Kuppers <lkuppers11@gmail.com>
2020-11-19 19:46:11 -0800

    UI tweaks to regster page

--- Tracker-Web/Model/Register/RegisterForm.cs
+++ Tracker-Web/Model/Register/RegisterForm.cs
@@ -9,21 +9,21 @@ namespace Tracker_Web.Model.Register
     public class RegisterForm
     {
         [Required]
-        [DataType(DataType.EmailAddress)]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Not a valid email address")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
-        [MaxLength(16)]
+        [MaxLength(16, ErrorMessage = "Username must have no greater than 16 characters")]
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Repeat Password required")]
         [DataType(DataType.Password)]
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string RepeatPassword { get; set; }
     }
 }
