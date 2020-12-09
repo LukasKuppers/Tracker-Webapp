3200dde89b3cf60bef12e0fdd5abcb2ad6f32b47
Lukas Kuppers <lkuppers11@gmail.com>
2020-12-08 19:21:16 -0800

    Add projects api consumer

---
+++ Tracker-Web/Services/EndpointConsumers/ProjectsConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker-Web.Services.EndpointConsumers
+{
+    public class ProjectsConsumer
+{
+}
+}
