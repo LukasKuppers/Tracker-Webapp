3e6de51795b471a0f38324a2677577e81c7f5c3c
Lukas Kuppers <lkuppers11@gmail.com>
2020-12-09 20:45:47 -0800

    Add create project endpoint consumer

--- Tracker-Web/Model/Project/PostProj.cs
+++ Tracker-Web/Model/Project/PostProj.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Tracker-Web.Model.Project
+namespace Tracker_Web.Model.Project
 {
     public class PostProjIn
-{
-}
+    {
+        public string Title { get; set; }
+    }
+
+    public class PostProjOut
+    {
+        public string Id { get; set; }
+    }
 }
--- Tracker-Web/Services/EndpointConsumers/ProjectsConsumer.cs
+++ Tracker-Web/Services/EndpointConsumers/ProjectsConsumer.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Tracker_Web.Model.Project;
 
-namespace Tracker-Web.Services.EndpointConsumers
+namespace Tracker_Web.Services.EndpointConsumers
 {
     public class ProjectsConsumer
-{
-}
+    {
+        private readonly IApiConsumer api;
+        
+        public ProjectsConsumer(HttpClient http, IJSRuntime js)
+        {
+            api = new Consumer(http, js);
+        }
+
+        public ProjectsConsumer(IApiConsumer api)
+        {
+            this.api = api;
+        }
+
+        public async Task<Project> GetProject(Guid projectId)
+        {
+            if (projectId == null)
+            {
+                return null;
+            }
+
+            var response = await api.MakeEmptyRequest<Project>(MethodType.GET, "/api/projects/" + projectId.ToString());
+            var code = response.Item2;
+
+            switch(code)
+            {
+                case System.Net.HttpStatusCode.OK:
+                    Project proj = response.Item1;
+                    return proj;
+                default:
+                    return null;
+            }
+        }
+
+        public async Task<Guid> CreateProject(string title)
+        {
+            if (title == null || title == "")
+            {
+                return Guid.Empty;
+            }
+
+            PostProjIn body = new PostProjIn() { Title = title };
+
+            var response = await api.MakeRequest<PostProjOut, PostProjIn>(MethodType.POST, "/api/projects", body);
+            var code = response.Item2;
+
+            switch(code)
+            {
+                case System.Net.HttpStatusCode.Created:
+                    string idRaw = response.Item1.Id;
+                    return Guid.Parse(idRaw);
+                default:
+                    return Guid.Empty;
+            }
+        }
+    }
 }
