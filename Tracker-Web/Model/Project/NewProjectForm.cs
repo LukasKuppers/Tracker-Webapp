3168bf81414f30974b646db3ce6f969fb13c5dea
Lukas Kuppers <lkuppers11@gmail.com>
2021-02-17 12:56:25 -0800

    Made create project modal functional

---
+++ Tracker-Web/Model/Project/NewProjectForm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tracker_Web.Model.Project
+{
+    public class NewProjectForm
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+    }
+}
